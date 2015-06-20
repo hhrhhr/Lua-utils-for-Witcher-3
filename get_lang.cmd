8b4c21fda7c44791c3585ae16f63a38e1a10d305
Dmitry Zaitsev <hhrhhr@gmail.com>
2015-06-20 11:44:32 +0300

    more automate

--- get_lang.cmd
+++ get_lang.cmd
@@ -1,32 +1,46 @@
 @echo off
 
-if .%1==. goto usage
-set lang=%1
-
 set w3dir=f:\lalala\The Witcher 3 Wild Hunt
 set outdir=g:\tmp\w3\lang
 
-set content=0 1 2 3 4 5 6 7 8 9 10 11 12
-set patch=0
-set dlc=1 2 3 4 5 7 8 13
 
 
-for %%i in (%content%) do (
-    echo content%%i
-    lua inspect_w3strings.lua "%w3dir%\content\content%%i\%lang%.w3strings" "%outdir%\content%%i_%lang%.txt"
-    echo.
+set languages=ru pl en de
+if .%1==. goto begin
+set languages=%1
+
+:begin
+for %%i in (%languages%) do (
+    rem echo %%i
+    set lang=%%i
+    call :convert
+)
+pause
+goto eof
+
+:convert
+for /l %%i in (0 1 16) do (
+    if exist "%w3dir%\content\content%%i\%lang%.w3strings" (
+        echo %lang% content%%i
+        lua inspect_w3strings.lua "%w3dir%\content\content%%i\%lang%.w3strings" "%outdir%\content%%i_%lang%.txt"
+        echo.
+    )
 )
 
-for %%i in (%patch%) do (
-    echo patch%%i
-    lua inspect_w3strings.lua "%w3dir%\content\patch%%i\%lang%.w3strings" "%outdir%\patch%%i_%lang%.txt"
-    echo.
+for /l %%i in (0 1 16) do (
+    if exist "%w3dir%\content\patch%%i\%lang%.w3strings" (
+        echo %lang% patch%%i
+        lua inspect_w3strings.lua "%w3dir%\content\patch%%i\%lang%.w3strings" "%outdir%\patch%%i_%lang%.txt"
+        echo.
+    )
 )
 
-for %%i in (%dlc%) do (
-    echo dlc%%i
-    lua inspect_w3strings.lua "%w3dir%\DLC\DLC%%i\content\%lang%.w3strings" "%outdir%\dlc%%i_%lang%.txt"
-    echo.
+for /l %%i in (0 1 16) do (
+    if exist "%w3dir%\DLC\DLC%%i\content\%lang%.w3strings" (
+        echo %lang% dlc%%i
+        lua inspect_w3strings.lua "%w3dir%\DLC\DLC%%i\content\%lang%.w3strings" "%outdir%\dlc%%i_%lang%.txt"
+        echo.
+    )
 )
 goto eof
 
@@ -37,4 +51,3 @@ echo     get_lang.cmd (de/en/ru/etc)
 echo.
 
 :eof
-pause
